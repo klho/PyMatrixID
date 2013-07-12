0400bb41df4f965af6ca49187e9673454993eb82
Kenneth L. Ho <kennethlho@gmail.com>
2013-07-12 13:15:05 -0700

    Updated to ID v0.3.

--- external/id_dist/development/idz_frm.f
+++ external/id_dist/development/idz_frm.f
@@ -401,14 +401,11 @@ c
 c       output:
 c       y -- transform of x
 c
-c       references:
-c       Liberty, Woolfe, Martinsson, Rokhlin, Tygert, "Randomized
-c            algorithms for the low-rank approximation of matrices,"
-c            Proceedings of the National Academy of Sciences USA,
-c            104 (51): 20167-20172, 2007.
-c       Woolfe, Liberty, Rokhlin, Tygert, "A fast randomized algorithm
-c            for the approximation of matrices," Applied and
-c            Computational Harmonic Analysis, to appear.
+c       reference:
+c       Halko, Martinsson, Tropp, "Finding structure with randomness:
+c            probabilistic algorithms for constructing approximate
+c            matrix decompositions," SIAM Review, 53 (2): 217-288,
+c            2011.
 c
         implicit none
         integer m,iw,n,k
@@ -471,14 +468,11 @@ c       y -- transform of x
 c
 c       _N.B._: l must be less than or equal to n.
 c
-c       references:
-c       Liberty, Woolfe, Martinsson, Rokhlin, Tygert, "Randomized
-c            algorithms for the low-rank approximation of matrices,"
-c            Proceedings of the National Academy of Sciences USA,
-c            104 (51): 20167-20172, 2007.
-c       Woolfe, Liberty, Rokhlin, Tygert, "A fast randomized algorithm
-c            for the approximation of matrices," Applied and
-c            Computational Harmonic Analysis, to appear.
+c       reference:
+c       Halko, Martinsson, Tropp, "Finding structure with randomness:
+c            probabilistic algorithms for constructing approximate
+c            matrix decompositions," SIAM Review, 53 (2): 217-288,
+c            2011.
 c
         implicit none
         integer m,iw,n,l
